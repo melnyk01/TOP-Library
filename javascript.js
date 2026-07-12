2a315b103d3b748c53b80a6a79abe73f1a2b773e
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-12 14:57:36 +0200

    Implement library rendering

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let books = [];
+const library = document.querySelector('.library');
 
 function Book(title, author, numberOfPages, status) {
     this.title = title;
@@ -9,15 +10,42 @@ function Book(title, author, numberOfPages, status) {
 }
 
 function addBookToLibrary(title, author, numberOfPages, status) {
-    Book.call(this, title, author, numberOfPages, status);
-    // books.push(book)
+    let book = new Book(title, author, numberOfPages, status);
+    books.push(book)
+
 }
 
-const book1 = new addBookToLibrary('hobbit', 'hh', 22, 'read');
-books.push(book1);
+function displayBook(book) {
+    const card = document.createElement('div');
+    const title = document.createElement('h1');
+    const author = document.createElement('h3');
+    const numberOfPages = document.createElement('p');
+    const status = document.createElement('p');
+    const removeBook = document.createElement('button');
+    title.textContent = book.title;
+    author.textContent = book.author;
+    numberOfPages.textContent = book.numberOfPages;
+    status.textContent = book.status;
+    removeBook.textContent = 'Remove from shelf';
+
+    library.appendChild(card);
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(numberOfPages);
+    card.appendChild(status);
+    card.appendChild(removeBook);
+}
+
+function displayLibrary() {
+    for (const book of books) {
+        displayBook(book);
+    }
+}
+
+
+addBookToLibrary('Red Rising', 'Pierce Brown', 382, 'finished');
+addBookToLibrary('Harry Potter', 'J. K. Rowling', 309, 'want to read');
 console.table(books)
 
-for (book of books) {
-
-}
+displayLibrary();
 
