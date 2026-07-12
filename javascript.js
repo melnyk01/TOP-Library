10cb81fb773a4ffc38eafbebd4564e5fec186057
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-12 17:35:43 +0200

    Add book creation form

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 let books = [];
 const library = document.querySelector('.library');
+const newBookButton = document.querySelector('#new-book');
+const dialog = document.querySelector('#new-book-dialog');
+const closeDialog = document.querySelector('#close-dialog');
+const formButton = document.querySelector('#form-button');
+const newBookTitle = document.querySelector('#book-title');
+const newBookAuthor = document.querySelector('#book-author');
+const newBookNumberOfPages = document.querySelector('#book-number-of-pages');
+const newBookStatus = document.querySelector('#book-status');
 
 function Book(title, author, numberOfPages, status) {
     this.title = title;
@@ -8,13 +16,11 @@ function Book(title, author, numberOfPages, status) {
     this.status = status;
     this.id = self.crypto.randomUUID()
 }
-
 function addBookToLibrary(title, author, numberOfPages, status) {
     let book = new Book(title, author, numberOfPages, status);
     books.push(book)
 
 }
-
 function displayBook(book) {
     const card = document.createElement('div');
     const title = document.createElement('h1');
@@ -35,13 +41,37 @@ function displayBook(book) {
     card.appendChild(status);
     card.appendChild(removeBook);
 }
-
 function displayLibrary() {
+    while (library.lastChild) {
+        library.removeChild(library.lastChild)
+    }
     for (const book of books) {
         displayBook(book);
     }
 }
 
+newBookButton.addEventListener('click', () => {
+    dialog.showModal();
+})
+closeDialog.addEventListener('click', () => {
+    dialog.close();
+})
+formButton.addEventListener('click', () => {
+    dialog.close();
+})
+formButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = newBookTitle.value;    
+    const author = newBookAuthor.value;    
+    const numberOfPages = newBookNumberOfPages.value;    
+    const status = newBookStatus.value;
+    newBookTitle.value = '';
+    newBookAuthor.value = '';
+    newBookNumberOfPages.value = '';
+    newBookStatus.value = '';
+    addBookToLibrary(title, author, numberOfPages, status);
+    displayLibrary()
+})
 
 addBookToLibrary('Red Rising', 'Pierce Brown', 382, 'finished');
 addBookToLibrary('Harry Potter', 'J. K. Rowling', 309, 'want to read');
