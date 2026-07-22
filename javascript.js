1738dc627667ae3521e1d97eeafa7f64e1b8d74e
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-22 22:28:24 +0200

    Refactor code to classes

--- javascript.js
+++ javascript.js
@@ -1,127 +1,130 @@
-let books = [];
-const library = document.querySelector('.library');
-const newBookButton = document.querySelector('#new-book');
-const dialog = document.querySelector('#new-book-dialog');
-const closeDialog = document.querySelector('#close-dialog');
-const formButton = document.querySelector('#form-button');
-const newBookTitle = document.querySelector('#book-title');
-const newBookAuthor = document.querySelector('#book-author');
-const newBookNumberOfPages = document.querySelector('#book-number-of-pages');
-const newBookStatus = document.querySelector('#book-status');
 
-function Book(title, author, numberOfPages, status) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.status = status;
-    this.id = self.crypto.randomUUID()
-}
-Book.prototype.changeStatus = function() {
-    switch (this.status) {
-        case 'Finished':
-            this.status = 'Want to read'
-            break;
-    
-        case 'Want to read':
-            this.status = 'Reading'
-            break;
-
-        case 'Reading':
-            this.status = 'Finished'
-            break;
+class Book {
+    constructor(title, author, numberOfPages, status) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.status = status;
+        this.id = self.crypto.randomUUID()
     }
-    displayLibrary();
-}
-function addBookToLibrary(title, author, numberOfPages, status) {
-    let book = new Book(title, author, numberOfPages, status);
-    books.push(book)
-
-}
-function displayBook(book) {
-    const card = document.createElement('div');
-    const title = document.createElement('h1');
-    const author = document.createElement('h3');
-    const numberOfPages = document.createElement('p');
-    const status = document.createElement('p');
-    const removeBook = document.createElement('button');
-    const changeStatus = document.createElement('button');
-    removeBook.id = book.id
-    changeStatus.id = book.id
-    title.textContent = book.title;
-    author.textContent = book.author;
-    numberOfPages.textContent = book.numberOfPages;
-    status.textContent = book.status;
-    removeBook.textContent = 'Remove from shelf';
-    changeStatus.textContent = 'Change book status';
-    changeStatus.className = 'status';
-
-    library.appendChild(card);
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(numberOfPages);
-    card.appendChild(status);
-    card.appendChild(removeBook);
-    card.appendChild(changeStatus);
-
-
-}
-function displayLibrary() {
-    while (library.lastChild) {
-        library.removeChild(library.lastChild)
+    changeBookStatus(bookId) {
+        for (let book of books) {
+            if (book.id == bookId) {
+                const bookIndex = myLibrary.books.indexOf(book);
+                myLibrary.books[bookIndex].changeStatus();
+            }
+        }
     }
-    for (const book of books) {
-        displayBook(book);
+
+
+}
+
+class UI {
+    constructor() {
+        this.uiLibrary = document.querySelector('.library');
+        this.newBookButton = document.querySelector('#new-book');
+        this.dialog = document.querySelector('#new-book-dialog');
+        this.closeDialog = document.querySelector('#close-dialog');
+        this.formButton = document.querySelector('#form-button');
+        this.newBookTitle = document.querySelector('#book-title');
+        this.newBookAuthor = document.querySelector('#book-author');
+        this.newBookNumberOfPages = document.querySelector('#book-number-of-pages');
+        this.newBookStatus = document.querySelector('#book-status');
+        this.newBookButton.addEventListener('click', () => {
+            this.dialog.showModal();
+        })
+        this.closeDialog.addEventListener('click', () => {
+            this.dialog.close();
+        })
+        this.formButton.addEventListener('click', () => {
+            this.dialog.close();
+        })
+        this.formButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            const title = this.newBookTitle.value;
+            const author = this.newBookAuthor.value;
+            const numberOfPages = this.newBookNumberOfPages.value;
+            const status = this.newBookStatus.value;
+            this.newBookTitle.value = '';
+            this.newBookAuthor.value = '';
+            this.newBookNumberOfPages.value = ''; 1
+            this.newBookStatus.value = '';
+            myLibrary.addBookToLibrary(title, author, numberOfPages, status);
+            myUI.displayLibrary();
+        })
+        this.uiLibrary.addEventListener('click', (e) => {
+            if (e.target.tagName !== 'BUTTON') return
+            if (e.target.className == 'status') {
+                changeBookStatus(e.target.id);
+            } else removeBook(e.target.id);
+
+        })
+
+
+    }
+    displayBook(book) {
+        const card = document.createElement('div');
+        const title = document.createElement('h1');
+        const author = document.createElement('h3');
+        const numberOfPages = document.createElement('p');
+        const status = document.createElement('p');
+        const removeBook = document.createElement('button');
+        const changeStatus = document.createElement('button');
+        removeBook.id = book.id
+        changeStatus.id = book.id
+        title.textContent = book.title;
+        author.textContent = book.author;
+        numberOfPages.textContent = book.numberOfPages;
+        status.textContent = book.status;
+        removeBook.textContent = 'Remove from shelf';
+        changeStatus.textContent = 'Change book status';
+        changeStatus.className = 'status';
+
+        this.uiLibrary.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(numberOfPages);
+        card.appendChild(status);
+        card.appendChild(removeBook);
+        card.appendChild(changeStatus);
+    }
+    displayLibrary() {
+        while (this.uiLibrary.lastChild) {
+            this.uiLibrary.removeChild(this.uiLibrary.lastChild)
+        }
+        for (const book of myLibrary.books) {
+            this.displayBook(book);
+            console.log(book);
+        }
     }
 }
-function removeBook(bookId) {
-    for (let book of books) {
-        if (book.id == bookId) {
-            const bookIndex = books.indexOf(book);
-            books.splice(bookIndex, 1);
-            displayLibrary()
+
+class Library {
+    constructor() {
+        this.books = []
+    }
+    addBookToLibrary(title, author, numberOfPages, status) {
+        let book = new Book(title, author, numberOfPages, status);
+        this.books.push(book)
+    }
+    removeBook(bookId) {
+        for (let book of this.books) {
+            if (book.id == bookId) {
+                const bookIndex = this.books.indexOf(book);
+                this.books.splice(bookIndex, 1);
+                myUI.displayLibrary();
+            }
         }
     }
 
 }
-function changeBookStatus(bookId) {
-    for (let book of books) {
-        if (book.id == bookId) {
-            const bookIndex = books.indexOf(book);
-            books[bookIndex].changeStatus();
-        }
-    }
-}
-newBookButton.addEventListener('click', () => {
-    dialog.showModal();
-})
-closeDialog.addEventListener('click', () => {
-    dialog.close();
-})
-formButton.addEventListener('click', () => {
-    dialog.close();
-})
-formButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const title = newBookTitle.value;    
-    const author = newBookAuthor.value;    
-    const numberOfPages = newBookNumberOfPages.value;    
-    const status = newBookStatus.value;
-    newBookTitle.value = '';
-    newBookAuthor.value = '';
-    newBookNumberOfPages.value = '';
-    newBookStatus.value = '';
-    addBookToLibrary(title, author, numberOfPages, status);
-    displayLibrary()
-})
-library.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'BUTTON') return
-    if (e.target.className == 'status') {
-        changeBookStatus(e.target.id);
-    } else removeBook(e.target.id);
 
-} )
 
-// addBookToLibrary('Red Rising', 'Pierce Brown', 382, 'Finished');
-// addBookToLibrary('Harry Potter', 'J. K. Rowling', 309, 'Finished');
+const myLibrary = new Library();
+const myUI = new UI();
+myLibrary.addBookToLibrary('Red Rising', 'Pierce Brown', 382, 'Finished');
+myLibrary.addBookToLibrary('Harry Potter', 'J. K. Rowling', 309, 'Finished');
 
-displayLibrary();
+
+console.log(myLibrary);
+myUI.displayLibrary();
